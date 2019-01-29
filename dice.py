0bbce08532c27dd134a7e619ed3484d9d1c4db50
Owen Mellema <owenmllm@gmail.com>
2019-01-29 17:18:52 -0600

    implement evaluate

--- dice.py
+++ dice.py
@@ -16,9 +16,73 @@ def roll(to_roll):
 	
 def _evaluateHelper(to_eval):
 	'''Pass in a string containing any dice string, except those containing a condition (to_eval), and return a number consistent with the query.'''
-
+	
 	#Convert all "-" into "+-"
-	to_eval_ready = 
-	pass
-def evaluate(to_eval, return_bool = False):
-	pass
+	to_eval_ready = "+-".join(to_eval.split('-'))
+
+	#Evaluate the new string.
+	total = 0
+	for statement in to_eval_ready.split('+'):
+		#See if the statement is a dice statement, by testing if there is a 'd' in it.
+		if "d" in statement:
+			total+=roll(statement)
+		else: #Otherwise, we assume that it is a constant.
+			total+=int(statement)
+	return total
+	
+def evaluate(to_eval, return_bool = False, failure_value = 0):
+	'''Evaluate a dice string, and return a number consistent with the query.
+	return_bool: Set this to true to return a boolean value instead of a number.
+	failure_value: The value to return on a failure.
+	'''
+	
+	if ">" in to_eval:
+		condChar = ">"
+	elif "<" in to_eval:
+		condChar = "<"
+	elif "=" in to_eval:
+		condChar = "="
+	else:
+		if return_bool:
+			return True
+		else:
+			return _evaluateHelper(to_eval)
+
+	#Get values before and after the conditional.
+	pre = _evaluateHelper(to_eval.split(condChar)[0])
+	suf = _evaluateHelper(to_eval.split(condChar)[1])
+
+	#Return an appropriate value based upon the conditional and the value of pre and suf.
+	if condChar == ">":
+		if pre > suf:
+			if return_bool:
+				return True
+			else:
+				return pre
+		else:
+			if return_bool:
+				return False
+			else:
+				return failure_value
+	elif condChar == "<":
+		if pre < suf:
+			if return_bool:
+				return True
+			else:
+				return pre
+		else:
+			if return_bool:
+				return False
+			else:
+				return failure_value
+	elif condChar == "=":
+		if pre == suf:
+			if return_bool:
+				return True
+			else:
+				return pre
+		else:
+			if return_bool:
+				return False
+			else:
+				return failure_value
