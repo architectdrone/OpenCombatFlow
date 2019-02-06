952f1db7a55fc9f8e3fae1498d7e2753f8ceaffc
Owen Mellema <owenmllm@gmail.com>
2019-02-06 17:06:43 -0600

    Create enforce.py, implement _enforceHelper

--- enforce.py
+++ enforce.py
@@ -1,29 +1,49 @@
 #Purpose: To enforce the format of blocks and dice-strings.
 import character
 
-def enforceDiceString(diceString):
-    #Not implemented
-    pass
+'''
+ENFORCEMENT BLOCK
+    (NOTE)
+    I decided not to add this to the DSD, because 1). It is only relevant for the purpose of rapid development of protocol enforcement, and 2). It has a list as the top level structure, not a dictionary.
 
-def enforceActionBlock(action):
-    #Make sure required keys are present.
-    assert 'range' in action, "ActionBlocks require range key (Type: RangeBlock)."
-    assert 'user' in action, "ActionBlocks require user key (Type: character)"
-    assert 'name' in action, "ActionBlocks require name key (Type: string)"
+    (LIST)
+    At the top level, the enforcementBlock is a list. Each element of the list corresponds to an element specified by the DSD. At each index in the list, there is a dictionary detailing information about the elemtn, as defined below.
+        (MANDATORY)
+        "name": The name of the key. (string)
+        "type": The type of the element at the key. This can be a type (eg, 'int', 'str', etc.) or a string, specifying a specific type of block.
 
-    #Make sure that correct types are given, and that dice strings are valid.
-    assert type(action['range']) == dict
-    assert type(action['user']) == character
-    assert type(action['name']) == str
-    assert type(action['damage']) == dict
-    for key in action['damage']:
-        assert type(action['damage'][key]) == str or type(action['damage'][key]==int)
-        enforceDiceString(action['damage'][key])
-    assert type(action['effects']) == dict
-    for key in action['effects']:
-        assert type(action['damage'][key]) == int or type(action['effects'][key]) == int
-        enforceDiceString(action['damage'][key])
-    assert type(action['chance']) == str
-    assert type(action['failureCondition']) == dict
-    
-    
+        (CONDITIONALLY MANDATORY)
+        "dictElement": What type of element each key in a dictionary must be of. Only nessesary if "type" is dict.
+
+        (NON-MANDATORY)
+        "mandatory": If true, this key must be present. Otherwise, or if it not provided, it is assumed to be non-mandatory.
+        
+'''
+def enforce(blockToCheck, blockType):
+    '''
+    Enforce rules for the given blockType.
+    '''
+
+def _enforceHelper(blockToCheck, enforcementBlock):
+    '''
+    PRIVATE: This is to enforce protocols using a pre-determined enforcement block. Please use an appropriate front-end channel to access this functionality.
+    '''
+
+    for keyToCheck in enforcementBlock:
+        #Check if mandatory elements are present.
+        if keyToCheck.get('mandatory', default=False) == True and keyToCheck['name'] not in blockToCheck:
+            raise KeyError(f"The Key {keyToCheck['name']} must be present in this block.")
+        elif keyToCheck['name'] not in blockToCheck:
+            continue
+
+        #Check if given values were consistent with required values.
+        if type(keyToCheck['type']) == type:
+            if type(blockToCheck[keyToCheck['name']]) != keyToCheck['type']: #Does the key in blockToCheck have the correct type?
+                raise KeyError(f"The Key {keyToCheck['name']} must be of type {keyToCheck['type']}, not of type {type(blockToCheck[keyToCheck['name']])}")
+            elif keyToCheck['type'] == dict: #Should we perform additional testing on the dictionary?
+                for internalKey in blockToCheck[keyToCheck['name']]:
+                    if type(blockToCheck[keyToCheck['name']][internalKey]) != keyToCheck['dictElement']:
+                        raise KeyError(f"The Key {internalKey} within the key {keyToCheck['name']} must be of type {keyToCheck['dictElement']}, not of type {type(blockToCheck[keyToCheck['name']][internalKey])}")
+        elif type(keyToCheck['type']) == str: #Should we do block testing?
+            enforce(blockToCheck[keyToCheck['name']], keyToCheck['type'])
+        
