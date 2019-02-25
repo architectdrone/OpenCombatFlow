7092f7c72fdcab6f272510450820cea319d4f42e
Owen Mellema <owenmllm@gmail.com>
2019-02-25 12:08:20 -0600

    Documented dice.py in a consistent manner

--- OpenCombatFlow/dice.py
+++ OpenCombatFlow/dice.py
@@ -1,7 +1,11 @@
 import random
 #See DiceStringFormat.txt for how to use dice strings.
-def roll(to_roll):
-	'''Pass in a string which contains only a dice statement (to_roll), and return a number consistent with the query.'''
+def _roll(to_roll):
+	'''
+	Pass in a string which contains only a dice statement (to_roll), and return a number consistent with the query.
+	@param to_roll A dice string in the form "XdY", where X and Y are integers.
+	@return The result of rolling the dice.
+	'''
 	numberDice = int(to_roll.split('d')[0])
 	numberSides = int(to_roll.split('d')[1])
 	
@@ -15,7 +19,11 @@ def roll(to_roll):
 	return total
 	
 def _evaluateHelper(to_eval):
-	'''Pass in a string containing any dice string, except those containing a condition (to_eval), and return a number consistent with the query.'''
+	'''
+	PRIVATE: A Helper function for evaluating dice strings.
+	@param to_eval A dice string with no conditionals.
+	@return the result of rolling the dice.
+	'''
 	
 	#Convert all "-" into "+-"
 	to_eval_ready = "+-".join(to_eval.split('-'))
@@ -25,7 +33,7 @@ def _evaluateHelper(to_eval):
 	for statement in to_eval_ready.split('+'):
 		#See if the statement is a dice statement, by testing if there is a 'd' in it.
 		if "d" in statement:
-			total+=roll(statement)
+			total+=_roll(statement)
 		elif statement == "":
 			pass
 		else: #Otherwise, we assume that it is a constant.
@@ -33,9 +41,12 @@ def _evaluateHelper(to_eval):
 	return total
 	
 def evaluate(to_eval, return_bool = False, failure_value = 0):
-	'''Evaluate a dice string, and return a number consistent with the query.
-	return_bool: Set this to true to return a boolean value instead of a number.
-	failure_value: The value to return on a failure.
+	'''
+	Evaluate a dice string, and return a number consistent with the query (IE, roll the dice)
+	@param to_eval The dice string to be evaluated. If this is an int, it will be returned with nothing else being done
+	@param return_bool Set this to true to return a boolean value instead of a number.
+	@param failure_value The value to return on a failure.
+	@return The result of rolling the dice.
 	'''
 	
 	if type(to_eval)==int:
