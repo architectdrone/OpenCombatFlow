e36c971a1493a1145ad81cd001322927e421c436
Owen Mellema <owenmllm@gmail.com>
2019-02-24 19:10:32 -0600

    Create setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='opencombatflow ',
+      version='1',
+      description='Tools for rapid development of RPGs',
+      url='https://github.com/architectdrone/OpenCombatFlow',
+      author='Owen Mellema',
+      license='MIT')
