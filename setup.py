7b87c3b1b5ebcf3cb3a162a6f2b210252780deab
Owen Mellema <owenmllm@gmail.com>
2019-02-26 12:22:29 -0600

    Add readme and tags to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+      with open("readme.md") as f:
+            return f.read()
+
 setup(name='opencombatflow',
       version='1.0',
       description='Tools for rapid development of RPGs',
       url='https://github.com/architectdrone/OpenCombatFlow',
       author='Owen Mellema',
-      license='MIT')
+      license='MIT',
+      keywords='games game rpg turn')
